ae92cbd8e1022c2a6b782d14e73974361cb1a870
rahul rawat <rahulrawathp2004@gmail.com>
2026-08-04 22:13:57 +0530

    Add root route to confirm backend is running

--- server/app.js
+++ server/app.js
@@ -15,7 +15,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/interview",interviewRouter);
