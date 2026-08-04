20ab2b4a498c9b774bcc11366289cbef83b7f613
rahul rawat <rahulrawathp2004@gmail.com>
2026-08-04 22:11:17 +0530

    Update server response message on startup
    Change console log to response message for backend status.

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const startServer = async () => {
         await connectDB();
 
         app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
+            res.send("Backend is running");
         });
     } catch (error) {
         console.error(error.message);
